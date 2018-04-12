d4632577580bdb3f7813eb35547a38705a3586c5
composerS <smy14520@126.com>
2018-04-12 14:13:07 +0800

    all

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -8,6 +8,6 @@
         <h1>Laravel-Create</h1>
         <p class="lead"></p>
         <p>一切从这里开始</p>
-        <p><a class="btn btn-success btn-lg" href="/register">现在注册</a></p>
+        <p><a class="btn btn-success btn-lg" href="{{route('signup')}}">现在注册</a></p>
     </div>
 @endsection
